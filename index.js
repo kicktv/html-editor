495d0c81d722b434d7675d086a3433fb1a23a2ba
khaledtv <khaledtv29@gmail.com>
2017-08-26 00:14:45 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+if (top != window) {top.location = window.location;}
+function preview(){temp=document.tester.box.value,preWindow=open("","preWindow"," menubar=yes, scrollbars=yes"),
+preWindow.document.open(),preWindow.document.write(temp),preWindow.document.close()}
+if (window.location.href.indexOf("www.html-editor.tk") != -1) {
+} else {window.location = "http://www.html-editor.tk/";}
+function urlCheck() 
+{if(window.location.href=="http://www.html-editor.tk/"){}else{
+(window.location='http://www.html-editor.tk/');}}
+urlCheck()
