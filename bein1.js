63a705bd9d7aad9786b38ef4ace32f25e52c079b
khaledtv <khaledtv29@gmail.com>
2018-12-30 01:10:53 +0100

    Update and rename bein1.js to bein1.html

--- bein1.js
+++
@@ -1,7 +0,0 @@
-if (typeof(width) == 'undefined') width = 600;
-if (typeof(height) == 'undefined') height = 400;
-if (width < 400) width = 400;
-if (height < 300) height = 300;
-document.write('<ifr'+'ame src="http://arembed.com/live.php?ch=Bein_Sports1" 
-marginheight="0" marginwidth="0" border="0" allowfullscreen="" 
-frameborder="0" height='+height+' scrolling="no" width='+width+'></ifr'+'ame>');
