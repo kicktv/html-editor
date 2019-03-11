be5c1d5ada47454c3a875de18ccae7a8edffb9b4
khaledtv <khaledtv29@gmail.com>
2019-03-11 18:20:52 +0100

    Update test.php

--- test.php
+++ test.php
@@ -1,4 +1,7 @@
-<?php  $url = $_GET['src'];
-$html = file_get_contents($url);
-echo $html;
+<?php
+$link = file_get_contents('http://www.mediafire.com/file/z0k7tchc7gts07m/drive.mp4');
+$start = strpos($link, 'http://download');
+$end = strpos($link, '"',$start);
+$link = substr($link, $start,$end - $start);
+echo $link;
 ?>
