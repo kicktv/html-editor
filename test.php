827744bfba88b9f375723c49f11ac176c95e0581
kicktv <khaledtv29@gmail.com>
2019-09-10 00:11:40 +0100

    Update test.php

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?php
-$link = file_get_contents('http://www.mediafire.com/file/z0k7tchc7gts07m/drive.mp4');
-$start = strpos($link, 'http://download');
+$link = file_get_contents('https://www.mediafire.com/file/z0k7tchc7gts07m/drive.mp4');
+$start = strpos($link, 'https://download');
 $end = strpos($link, '"',$start);
 $link = substr($link, $start,$end - $start);
 echo $link;
