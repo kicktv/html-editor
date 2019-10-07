db2af138ee4df3a4cb2ff4c9d32a083923c0c65b
kicktv <khaledtv29@gmail.com>
2019-10-07 11:33:53 +0100

    Update and rename test.php to test.html

--- test.php
+++
@@ -1,7 +0,0 @@
-<?php
-$link = file_get_contents('https://www.mediafire.com/file/z0k7tchc7gts07m/drive.mp4');
-$start = strpos($link, 'https://download');
-$end = strpos($link, '"',$start);
-$link = substr($link, $start,$end - $start);
-echo $link;
-?>
